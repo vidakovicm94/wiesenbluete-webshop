d01a202bdf4a34aa12086d5995d2be0a445992fa
vidakovicm94 <122536524+vidakovicm94@users.noreply.github.com>
2023-09-25 18:47:14 +0200

    Add files via upload

---
+++ src/main/java/com/example/wiesenbluetewebshop/WiesenblueteWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.wiesenbluetewebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiesenblueteWebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiesenblueteWebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wiesenbluetewebshop/WiesenblueteWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wiesenbluetewebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiesenblueteWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
